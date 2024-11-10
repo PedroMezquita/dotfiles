93272b5be0dc089cd80e4a721fe1ff5e9c9ade51
Pedro Mezquita <pedramapedrama@gmail.com>
2024-11-10 16:28:30 +0100

    Added screen lock shortcuts and shortcut for screen switch changed

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -50,6 +50,7 @@ beautiful.init(string.format("%s/.config/awesome/themes/%s/theme.lua", os.getenv
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvt"
+lockscreen = "i3lock"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
@@ -259,10 +260,12 @@ globalkeys = gears.table.join(
               {description = "swap with next client by index", group = "client"}),
     awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end,
               {description = "swap with previous client by index", group = "client"}),
-    awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative( 1) end,
+    awful.key({ modkey,  }, "Up", function () awful.screen.focus_relative( 1) end,
               {description = "focus the next screen", group = "screen"}),
-    awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end,
+    awful.key({ modkey, }, "Down", function () awful.screen.focus_relative(-1) end,
               {description = "focus the previous screen", group = "screen"}),
+    awful.key({ modkey, }, "l", function () awful.spawn(lockscreen) end,
+    	       {description = "Locks the screen with i3lock", group = "screen"}),
     awful.key({ modkey,           }, "u", awful.client.urgent.jumpto,
               {description = "jump to urgent client", group = "client"}),
     awful.key({ modkey,           }, "Tab",
